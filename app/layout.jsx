0be6fba562681adf3870728c16bf9751ff53aec0
ayopaul <ayoblurg@gmail.com>
2026-03-02 23:16:14 +0000

    gtag

--- app/layout.jsx
+++ app/layout.jsx
@@ -15,25 +15,31 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <head>
         <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-5MBBFQBG');`
+          }}
+        />
+        <script
           id="Cookiebot"
           src="https://consent.cookiebot.com/uc.js"
           data-cbid="e551b348-8918-4a94-be1c-f6f751ac25fc"
           type="text/javascript"
           async
         />
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-BCNLP50H4K" />
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'G-BCNLP50H4K');
-            `
-          }}
-        />
       </head>
       <body>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-5MBBFQBG"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
         {children}
         <Analytics />
       </body>
